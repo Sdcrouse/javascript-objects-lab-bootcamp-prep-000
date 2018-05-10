6b70e2e5d76589d70ce6407f4d53270eafdabdf1
Steven Crouse <sdcrouse91@gmail.com>
2018-05-10 06:02:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var recipes = {eggs: 2, flour: "2 cups", sugar: "1 cup", milk: "1/2 cup", chocolate: "2/3 cup"};
+
+function updateObjectWithKeyAndValue (object, key, value){
+  return Object.assign({}, object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue (obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
